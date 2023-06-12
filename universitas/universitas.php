006a3a2b279a53d04a23056d19840a8192524239
arjunatanjung <127956422+arjunatanjung@users.noreply.github.com>
2023-06-12 16:30:56 +0700

    Add files via upload

---
+++ universitas/universitas.php
@@ -0,0 +1,85 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <link rel="stylesheet" href="style.css">
+    <link href="logo.png" rel="shortcut icon">
+    <title>hogwarts university</title>
+</head>
+<body>
+<!-- navbar -->
+<header>
+<nav class="navbar navbar-expand-lg">
+  <div class="container">
+    <a class="navbar-brand" href="#"><img src="logo.png" width="100px" alt=""></a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+      <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" id="navbarDarkDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false" style="color:white;">
+            news
+          </a>
+          <ul class="dropdown-menu dropdown-menu-dark" aria-labelledby="navbarDarkDropdownMenuLink">
+            <li><a class="dropdown-item" href="#">events</a></li>
+            <li><a class="dropdown-item" href="#">fatures</a></li>
+            <li><a class="dropdown-item" href="#">new release</a></li>
+          </ul>
+        </li>
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" id="navbarDarkDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false"style="color:white;">
+            about
+          </a>
+          <ul class="dropdown-menu dropdown-menu-dark" aria-labelledby="navbarDarkDropdownMenuLink">
+            <li><a class="dropdown-item" href="#">organisation</a></li>
+            <li><a class="dropdown-item" href="#">university</a></li>
+            <li><a class="dropdown-item" href="#">jobs</a></li>
+            <li><a class="dropdown-item" href="#">lecturer</a></li>
+          </ul>
+        </li>
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" id="navbarDarkDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false"style="color:white;">
+            admissions
+          </a>
+          <ul class="dropdown-menu dropdown-menu-dark" aria-labelledby="navbarDarkDropdownMenuLink">
+            <li><a class="dropdown-item" href="#">graduate</a></li>
+            <li><a class="dropdown-item" href="#">undergraduate</a></li>
+            <li><a class="dropdown-item" href="#">continuing education</a></li>
+          </ul>
+        </li>
+      </ul>
+      <form class="d-flex">
+      <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#" style="color:white;">contact us</a>
+        </li>
+      </form>
+    </div>
+  </div>
+</nav>
+</header>
+<!-- nav end -->
+<!-- carousel -->
+<section class="hero" id="hero">
+<div id="carouselExampleControls" class="carousel slide" data-bs-ride="carousel">
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+     <div class="carousel-background">
+      <img src="town.jpg" alt="">
+      <div class="carousel-container">
+      <div class="carousel-content-container">
+      <h2 style="color:white;">welcome to hogwarts university </h2>
+      </div>
+      </div>
+     </div>
+  </div>
+</div>
+</section>
+<!-- carousel end -->
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+</body>
+</html>
